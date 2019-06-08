d76861b5f10627038ebd1e45baa61f110b00a409
Rob Taussig <robert.taussig@gmail.com>
2019-06-08 15:12:23 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useLoadWasm, useWasm, WASM_READY_STATE } from './useWasm';
 
-function App() {
+const TestComponent = () => {
+  const { wasm, readyState } = useWasm();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={wasm.greet}>Click</button>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [WasmProvider, wasmObject] = useLoadWasm();
+
+  return (
+    <WasmProvider value={wasmObject}>
+      <div id="App">
+        {wasmObject.readyState === WASM_READY_STATE.READY && <TestComponent/>}
+      </div>
+    </WasmProvider>
+  );
+};
 
 export default App;
---
+++ src/useWasm/index.js
@@ -0,0 +1,34 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+
+export const WasmContext = createContext();
+export const WASM_READY_STATE = {
+  ERROR: -2,
+  NOT_READY: 0,
+  READY: 1,
+};
+
+export const useLoadWasm = () => {
+  const [wasmObject, setWasmObject] = useState({ wasm: null, readyState: WASM_READY_STATE.NOT_READY });
+
+  useEffect(() => {
+    const loadWasm = async () => {
+      try {
+        const wasm = await import('wasm');
+        setWasmObject({ wasm, readyState: WASM_READY_STATE.READY });
+      } catch(e) {
+        console.error(e);
+        setWasmObject({ wasm: null, readyState: WASM_READY_STATE.ERROR });
+      }
+    };
+
+    loadWasm();
+  }, []);
+
+  return [WasmContext.Provider, wasmObject];
+};
+
+export const useWasm = () => {
+  const { wasm, readyState } = useContext(WasmContext);
+
+  return { wasm, readyState };
+};
